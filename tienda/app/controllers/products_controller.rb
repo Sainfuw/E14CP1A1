e5625373e392f3b74b40d8d71ed29b9d0cd6b790
Sainfuw <sainfuw@aol.com>
2018-07-30 16:41:29 -0400

    create, remove and list products on category show

--- tienda/app/controllers/products_controller.rb
+++ tienda/app/controllers/products_controller.rb
@@ -1,2 +1,17 @@
 class ProductsController < ApplicationController
+  def create
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to @product.category
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to @product.category
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :price, :category_id)
+  end
 end
